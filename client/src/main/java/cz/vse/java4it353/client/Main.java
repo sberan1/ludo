6a65657806033bad9825512ece516d8565279b3c
Sekera-Alexandr <120587451+Sekera-Alexandr@users.noreply.github.com>
2024-05-08 15:02:23 +0200

    Zprovoznění vzhledu hlavní aplikace a jejího spouštění

--- client/src/main/java/cz/vse/java4it353/client/Main.java
+++ client/src/main/java/cz/vse/java4it353/client/Main.java
@@ -1,12 +1,16 @@
 package cz.vse.java4it353.client;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 
 public class Main extends Application {
@@ -18,11 +22,14 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("Hello, JavaFX!");
-        StackPane root = new StackPane(label);
-        Scene scene = new Scene(root, 300, 200);
+    public void start(Stage primaryStage) throws IOException {
+        // Načtení FXML souboru
+        Parent root = FXMLLoader.load(getClass().getResource("/aplikace.fxml"));
 
+        // Vytvoření scény
+        Scene scene = new Scene(root, 703, 980);
+
+        // Nastavení scény a zobrazení hlavního okna
         primaryStage.setTitle("JavaFX Application");
         primaryStage.setScene(scene);
         primaryStage.show();
