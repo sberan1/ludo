f4eba8ec002b55cb2b9480db0e96b3d6b3019bf6
Sekera-Alexandr <120587451+Sekera-Alexandr@users.noreply.github.com>
2024-05-08 13:05:07 +0200

    Test branche

--- client/src/main/java/cz/vse/java4it353/client/Main.java
+++ client/src/main/java/cz/vse/java4it353/client/Main.java
@@ -11,5 +11,6 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        //Změna v Aleksovi
     }
 }
