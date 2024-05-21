0e06c5f6575385b67b41cba90367182e67127908
Sekera-Alexandr <120587451+Sekera-Alexandr@users.noreply.github.com>
2024-05-21 15:11:38 +0200

    Přidání neviditelných ImageViews pro pohyb figurek

--- client/src/main/java/cz/vse/java4it353/client/Client.java
+++ client/src/main/java/cz/vse/java4it353/client/Client.java
@@ -3,7 +3,9 @@ package cz.vse.java4it353.client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.HashMap;
@@ -28,13 +30,21 @@ public class Client {
 
     public void send(String data) {
         try (
-                Socket clientSocket = new Socket("127.0.0.1", 8080);
+                Socket clientSocket = new Socket("127.0.0.1", 12345);
                 PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         ) {
             Thread listenerThread = new Thread(new Listener(clientSocket.getInputStream()));
             listenerThread.start();
 
             pw.println(data);
+
+            String odpoved;
+            while((odpoved = in.readLine()) != null)
+            {
+                logger.info(odpoved);
+            }
+
         } catch (IOException e) {
             logger.error("Doslo k vyjimce behem komunikace se serverem.", e);
         }
--- client/src/main/java/cz/vse/java4it353/client/HomeController.java
+++ client/src/main/java/cz/vse/java4it353/client/HomeController.java
@@ -54,6 +54,118 @@ public class HomeController {
     @FXML
     private ImageView figurka4L;
     private ImageView selectedFigurka = null;
+    @FXML
+    private ImageView c1xl31xm21xz11;
+    @FXML
+    private ImageView c2xl32xm22xz12;
+    @FXML
+    private ImageView c3xl33xm23xz13;
+    @FXML
+    private ImageView c4xl34xm24xz14;
+    @FXML
+    private ImageView c5xl35xm25xz15;
+    @FXML
+    private ImageView c6xl36xm26xz16;
+    @FXML
+    private ImageView c7xl37xm27xz17;
+    @FXML
+    private ImageView c8xl38xm28xz18;
+    @FXML
+    private ImageView c9xl39xm29xz19;
+    @FXML
+    private ImageView c10xl40xm30xz20;
+    @FXML
+    private ImageView c11xl1xm31xz21;
+    @FXML
+    private ImageView c12xl2xm32xz22;
+    @FXML
+    private ImageView c13xl3xm33xz23;
+    @FXML
+    private ImageView c14xl4xm34xz24;
+    @FXML
+    private ImageView c15xl5xm35xz25;
+    @FXML
+    private ImageView c16xl6xm36xz26;
+    @FXML
+    private ImageView c17xl7xm37xz27;
+    @FXML
+    private ImageView c18xl8xm38xz28;
+    @FXML
+    private ImageView c19xl9xm39xz29;
+    @FXML
+    private ImageView c20xl10xm40xz30;
+    @FXML
+    private ImageView c21xl11xm1xz31;
+    @FXML
+    private ImageView c22xl12xm2xz32;
+    @FXML
+    private ImageView c23xl13xm3xz33;
+    @FXML
+    private ImageView c24xl14xm4xz34;
+    @FXML
+    private ImageView c25xl15xm5xz35;
+    @FXML
+    private ImageView c26xl16xm6xz36;
+    @FXML
+    private ImageView c27xl17xm7xz37;
+    @FXML
+    private ImageView c28xl18xm8xz38;
+    @FXML
+    private ImageView c29xl19xm9xz39;
+    @FXML
+    private ImageView c30xl20xm10xz40;
+    @FXML
+    private ImageView c31xl21xm11xz1;
+    @FXML
+    private ImageView c32xl22xm12xz12;
+    @FXML
+    private ImageView c33xl23xm13xz3;
+    @FXML
+    private ImageView c34xl24xm14xz4;
+    @FXML
+    private ImageView c35xl25xm15xz5;
+    @FXML
+    private ImageView c36xl26xm16xz6;
+    @FXML
+    private ImageView c37xl27xm17xz7;
+    @FXML
+    private ImageView c38xl28xm18xz8;
+    @FXML
+    private ImageView c39xl29xm19xz9;
+    @FXML
+    private ImageView c40xl30xm20xz10;
+    @FXML
+    private ImageView c41;
+    @FXML
+    private ImageView c42;
+    @FXML
+    private ImageView c43;
+    @FXML
+    private ImageView c44;
+    @FXML
+    private ImageView m44;
+    @FXML
+    private ImageView m43;
+    @FXML
+    private ImageView m42;
+    @FXML
+    private ImageView m41;
+    @FXML
+    private ImageView l41;
+    @FXML
+    private ImageView l42;
+    @FXML
+    private ImageView l43;
+    @FXML
+    private ImageView l44;
+    @FXML
+    private ImageView z44;
+    @FXML
+    private ImageView z43;
+    @FXML
+    private ImageView z42;
+    @FXML
+    private ImageView z41;
     public void initialize() {
         figurka1C.setOnMouseClicked(this::handleFigurkaClick);
         figurka2C.setOnMouseClicked(this::handleFigurkaClick);
@@ -74,19 +186,22 @@ public class HomeController {
     }
     private void handleFigurkaClick(MouseEvent event) {
         selectedFigurka = (ImageView) event.getSource();
+        String command = "M ";// + výsledek hodu kostkou
+        Client client = Client.getInstance();
+        client.send(command);
     }
     public void hodKostkou(ActionEvent actionEvent) {
         String command = "R";
         Client client = Client.getInstance();
         client.send(command);
     }
-    private void posunFigurkou(MouseEvent event) {
-        if (selectedFigurka != null) {
-            ImageView clickedPosition = (ImageView) event.getSource();
-            String command = "M " + selectedFigurka.getId() + " " + clickedPosition.getId();
-            Client client = Client.getInstance();
-            client.send(command);
-        }
+    private void handlePositionClick(MouseEvent event) {
+        // Posun figurky na pozici, kterou mi vrátí server - musím si jí vypočítat sám
+        // c1xl31xm21xz11
+        // c1 = Červená barva 1. políčko
+        // l31 = Žlutá barva 31. políčko
+        // m21 = Modrá barva 21. políčko
+        // z11 = Zelená barva 11. políčko
     }
 
     public void odeslat(ActionEvent actionEvent) {
